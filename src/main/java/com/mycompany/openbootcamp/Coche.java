eb2bfffe03dcfd867783ac0e517d06facbf8d5c7
Sebastian <sebas.tesitore@gmail.com>
2022-09-24 15:49:05 -0300

    ejercicio tema 3

---
+++ src/main/java/com/mycompany/openbootcamp/Coche.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.openbootcamp;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class Coche {
+   public int puertas = 4;
+   
+   public void incrementarPuertas(){
+       this.puertas ++;
+   }
+}
--- src/main/java/com/mycompany/openbootcamp/OpenBootcamp.java
+++ src/main/java/com/mycompany/openbootcamp/OpenBootcamp.java
@@ -12,6 +12,28 @@ package com.mycompany.openbootcamp;
 public class OpenBootcamp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        
+        int resultado = sumar3(a,b,c);
+        System.out.println("El resultado de sumar 'a', 'b' y 'c' es: " + resultado);
+        
+        Coche miCoche = new Coche();
+        System.out.println("miCoche tiene: " + miCoche.puertas + " puertas");
+        miCoche.incrementarPuertas();
+        System.out.println("Luego de llamar a incrementarPuertas() ahora miCoche tiene: " + miCoche.puertas +" puertas");
+        
     }
+    
+  
+    public static int sumar3(int num1,int num2, int num3){
+
+        int resultado = num1 + num2 + num3;
+        return resultado;
+    };
+    
+    
+    
 }
+
